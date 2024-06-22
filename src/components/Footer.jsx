7b56d35f522f465b8d104f44e46ce0445608762f
weshare237 <fopaduclair2000@gmail.com>
2024-06-22 06:49:20 +0100

    added click with connect wallet on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
+import { useConnectWallet } from '@web3-onboard/react'
 
 const Footer = () => {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+
   return (
     <div className='w-full dark:bg-gray-800'>
       <div className='max-w-2xl mx-auto text-white py-10'>
         <div className='text-center'>
           <h3 className='text-4xl lg:text-5xl font-extrabold text-white mb-6 md:!leading-[55px] text-center'>
-            {' '}
-            Download our app{' '}
+            Download our app
           </h3>
           <p> Trade Strong. All Markets, Every Moment. </p>
           <div className='flex justify-center my-10'>
-            <div className='flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2'>
+            <div
+              onClick={() => connect()}
+              className='flex items-center border rounded-lg px-4 py-2 w-52 mx-2 hover:bg-gray-700 transition duration-300 ease-in-out cursor-pointer'
+            >
               <img
                 src='https://cdn-icons-png.flaticon.com/512/888/888857.png'
                 className='w-7 md:w-8'
-                alt='footer icon'
+                alt='Google Play Store'
               />
               <div className='text-left ml-3'>
                 <p className='text-xs text-gray-200'>Download on </p>
                 <p className='text-sm md:text-base'> Google Play Store </p>
               </div>
             </div>
-            <div className='flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2'>
+            <div
+              onClick={() => connect()}
+              className='flex items-center border rounded-lg px-4 py-2 w-44 mx-2 hover:bg-gray-700 transition duration-300 ease-in-out cursor-pointer'
+            >
               <img
                 src='https://cdn-icons-png.flaticon.com/512/888/888841.png'
                 className='w-7 md:w-8'
-                alt=''
+                alt='Apple Store'
               />
               <div className='text-left ml-3'>
                 <p className='text-xs text-gray-200'>Download on </p>
@@ -37,13 +44,27 @@ const Footer = () => {
         </div>
         <div className='mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-white'>
           <p className='order-2 md:order-1 mt-8 md:mt-0'>
-            {' '}
-            &copy; QuickSwap, 2024.{' '}
+            &copy; QuickSwap, 2024.
           </p>
           <div className='order-1 md:order-2'>
-            <span className='px-2'>About us</span>
-            <span className='px-2 border-l'>Contact us</span>
-            <span className='px-2 border-l'>Privacy Policy</span>
+            <span
+              className='px-2 hover:text-gray-400 transition duration-300 ease-in-out cursor-pointer'
+              onClick={() => connect()}
+            >
+              About us
+            </span>
+            <span
+              onClick={() => connect()}
+              className='px-2 border-l hover:text-gray-400 transition duration-300 ease-in-out cursor-pointer'
+            >
+              Contact us
+            </span>
+            <span
+              onClick={() => connect()}
+              className='px-2 border-l hover:text-gray-400 transition duration-300 ease-in-out cursor-pointer'
+            >
+              Privacy Policy
+            </span>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import './index.css'
 import { Web3OnboardProvider, init } from '@web3-onboard/react'
 import walletConnectModule from '@web3-onboard/walletconnect'
 import coinbaseWalletModule from '@web3-onboard/coinbase'
-import infinityWalletWalletModule from '@web3-onboard/infinity-wallet'
 
 const INFURA_KEY = import.meta.env.VITE_INFURA_ID
 
@@ -28,16 +27,37 @@ const wcInitOptions = {
 }
 
 const walletConnect = walletConnectModule(wcInitOptions)
-const infinityWalletSDK = infinityWalletWalletModule()
 
 const coinbaseWalletSdk = coinbaseWalletModule({ darkMode: true })
 
 const web3Onboard = init({
-  wallets: [walletConnect, coinbaseWalletSdk, infinityWalletSDK],
+  wallets: [walletConnect, coinbaseWalletSdk],
   chains,
   appMetadata: {
     name: 'QuickSwap',
-    icon: '<svg>QuickSwap</svg>',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" version="1.1" viewBox="0 0 16.933 16.933">
+            <g transform="translate(-43.614 -50.361)">
+              <rect x="44.342" y="51.071" width="15.482" height="15.482" fill="#fff" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3229"/>
+              <g stroke-width="0">
+              <circle cx="52.082" cy="58.851" r="1.2191"/>
+              <circle cx="46.868" cy="53.896" r="1.2191"/>
+              <circle cx="57.408" cy="64.003" r="1.2191"/>
+              </g>
+            </g>
+            <g transform="matrix(.93887 0 0 .93887 -118.66 -82.02)">
+              <text x="78.373802" y="66.259865" fill="#000000" font-family="Impact" font-size="8.9737px" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" xml:space="preserve"><tspan x="78.373802" y="66.259865" font-family="Impact" stroke-width="0">DICESWAP</tspan></text>
+              <g transform="matrix(.12685 0 0 .12685 73.895 51.762)">
+              <g transform="translate(15.652 -10.343)">
+                <rect x="61.027" y="51.683" width="15.482" height="15.482" fill="#fff" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3229"/>
+                <g stroke-width="0">
+                <circle cx="69.059" cy="59.463" r="1.2191"/>
+                <circle cx="63.553" cy="54.508" r="1.2191"/>
+                <circle cx="74.093" cy="64.615" r="1.2191"/>
+                </g>
+              </g>
+              </g>
+            </g>
+          </svg>`,
     description:
       'QuickSwap is a cryptocurrency exchange that allows to swap BTC and altcoins in an easy way. DiceSwap supports 1000 cryptocurrencies. Make Bitcoin to Ethereum, Litecoin crypto exchanges at the best rates!',
   },
